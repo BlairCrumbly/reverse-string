84cbfe3b5f26aa7d88f16a990e6123a7714c9cdf
Blair Crumbly <blaircrumbly@gmail.com>
2025-04-27 19:29:56 -0400

    overall good pseudocoding and use of methods.

--- javascript/reverse_string.js
+++ javascript/reverse_string.js
@@ -1,9 +1,28 @@
+// if str = None return
+//create reversedstring empty array
+// Iterate through the string from the end, using the length of the string +1 inclusive
+// push each element to a new array (reversedstring)
+//return reversedstring
+
+
 function reverseString(str) {
-  // type your code here
+  if(str == []){
+    return
+  }
+  let reversedstring = []
+  for (let i = str.length + 1; i > -1; i--){
+    reversedstring.push(str[i])
+  }
+  return reversedstring.join("").toString()
+
 }
 
 if (require.main === module) {
   // add your own tests in here
+  console.log("Expecting 'tac'")
+  console.log("=>", reverseString("cat"));
+  console.log("Expecting racecaR");
+  console.log("=>", reverseString("Racecar"));
   console.log("Expecting: 'ih'");
   console.log("=>", reverseString("hi"));
 
